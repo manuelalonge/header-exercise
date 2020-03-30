e0de6467369a671835fcd53bb9e24c20ebf75ca9
Manuel Alonge <manuel.alonge@gmail.com>
2020-03-30 15:52:39 +0200

    hamburger added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Test to make sure that the file works
 console.log('index.js works');
 
-// Function below changes navbar background color on mouse scroll
+// Function below changes navbar background color on mouse scroll (JQuery involved)
 
 $(function () {
     $(window).on("scroll", function () {
         if ($(window).scrollTop() > 50) {
             $(".header").addClass("active");
         } else {
-            //remove the background property so it comes transparent again (defined in your css)
+            // remove the background property so it comes transparent again (defined in your css)
             $(".header").removeClass("active");
         }
     });
